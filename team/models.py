acde5ca744c5525aeae747368d1ec28dce82d1a0
Mohit Solanki <mohitsolanki2605@gmail.com>
2019-03-25 22:27:04 +0530

    Made captain uneditable

--- team/models.py
+++ team/models.py
@@ -5,7 +5,7 @@ class Team(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=20)
     budget_available = models.FloatField(editable=False, default=100.0)
-    captain = models.ForeignKey('player.Player', null=True, on_delete=models.SET_NULL)
+    captain = models.ForeignKey('player.Player', editable=False, null=True, on_delete=models.SET_NULL)
     spots_available = models.IntegerField(editable=False, default=5)
 
     class Meta:
