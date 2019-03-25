9c37a1b60fa784e538dce24f20524d1440c43850
Mohit Solanki <mohitsolanki2605@gmail.com>
2019-03-25 23:58:51 +0530

    Permissions created

--- team/permissions.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import permissions
-
-class IsOwnerOrAdmin(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return (request.user == obj.owner) or (request.user.is_authenticated and request.user.is_superuser)
